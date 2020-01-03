12ae80cb3f5c31919b59e15a4d7d069476c28446
Vadim Volkov <triv@me.com>
2020-01-03 09:25:43 -0500

    Empty cart + wait conditions

--- src/test/java/StainlessWorkTableSearch.java
+++ src/test/java/StainlessWorkTableSearch.java
@@ -9,9 +9,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.sound.midi.Soundbank;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StainlessWorkTableSearch {
@@ -30,11 +33,12 @@ public class StainlessWorkTableSearch {
         driver.get("https://www.webstaurantstore.com");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
     }
 
     @AfterEach
     void tearDown() throws Exception {
-        driver.quit();
+       driver.quit();
     }
 
     @Test
@@ -46,8 +50,11 @@ public class StainlessWorkTableSearch {
         WebElement searchBtn = driver.findElement(By.cssSelector("[value='Search']"));
         searchBox.sendKeys(searchValue);
         searchBtn.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
         // compare the expected title of result page with actual title
-        assertTrue(driver.findElement(By.xpath("//div[@id='search-elastic']//*[contains(text(),'Search Results for:')]")).getText().toLowerCase().contains(searchValue));
+        assertTrue(driver.findElement(By.xpath("//div[@id='search-elastic']//*[contains(text(),'Search Results for:')]"))
+                .getText().toLowerCase().contains(searchValue));
 
         //get pagination length
         int paginationLength;
@@ -62,54 +69,37 @@ public class StainlessWorkTableSearch {
         WebElement lastItem = null;
         // check all result on each paginate
         for(int i = 1; i <= paginationLength; i++) {
-            Thread.sleep(1000);// wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#product_listing")));
+            (new WebDriverWait(driver, 3)).until(
+                    ExpectedConditions.presenceOfElementLocated(By.cssSelector("#product_listing")));
             List<WebElement> pageSearchResults = driver.findElements(By.cssSelector("div.details > a.description"));
             lastItem = pageSearchResults.get(pageSearchResults.size()-1);
             for(WebElement element: pageSearchResults) {
-                assertTrue(element.getText().contains(checkValue), element.getText());
+                assertTrue(element.getText().contains(checkValue), lastItem.getText());
             }
             WebElement nextBtn = driver.findElement(By.cssSelector(".icon-right-open"));
             nextBtn.click();
         }
 
         //Add the last item into the cart
+        assert lastItem != null;
         lastItem.click();
-        //WebDriverWait wait = new WebDriverWait(driver,30);
-        Thread.sleep(1000); // wait.until(ExpectedConditions.presenceOfElementLocated(By.id("#buyButton")));
+        (new WebDriverWait(driver, 3)).until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='buyButton']")));
         driver.findElement(By.xpath("//input[@id='buyButton']")).click();
-        driver.findElement(By.xpath("//span[contains(text(),'Cart')]")).click();
+        (new WebDriverWait(driver, 3)).until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='subject']/div[2]/div/a[1]/button")));
+        driver.findElement(By.xpath("//*[@id='subject']/div[2]/div/a[1]/button")).click();
 
-        Thread.sleep(5000);
-        //(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#product_listing")));
-
-
-
-
-
-        /*
-        * for(for (WebElement ele: PageSearchResults){}
-         * WebElement nextBtn =
-         * driver.findElement(By.xpath("//*[@class='icon-right-open']"));
-         *
-         * List<WebElement> AllSearchResults =
-         * driver.findElements(By.xpath("//div[@id]//a[@class='description']")); for(int
-         * i=1; i<=9; i++) { //AllSearchResults.addAll(driver.findElements(By.xpath(
-         * "//div[@id]//a[@class='description']"))); nextBtn.click(); }
-         *
-         * //int numberOfLinks = AllSearchResults.size();
-         * //System.out.println(numberOfLinks); for(WebElement element:
-         * AllSearchResults) System.out.println(element.getText());
-         *
-         * // xpath: //div[@id="product_listing"]//a[@href] - wrong //
-         *
-         * //List<WebElement> pagination =
-         * driver.findElements(By.xpath("//div[@id='paging']//a"));;
-         */
-
+        //Empty cart
+        (new WebDriverWait(driver, 3)).until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='main']/div[3]/form/div/div[1]/div/a")));
+        driver.findElement(By.xpath("//*[@id='main']/div[3]/form/div/div[1]/div/a")).click();
+        (new WebDriverWait(driver, 3)).until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(),'Empty Cart')]")));
+        driver.findElement(By.xpath("//button[contains(text(),'Empty Cart')]")).click();
+        (new WebDriverWait(driver, 3)).until(
+                ExpectedConditions.textToBePresentInElementLocated(By.xpath("//p[@class='header-1']"), "Your cart is empty."));
+        //(new WebDriverWait(driver, 3)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[@class='header-1']")));
 
     }
-
-
-
-
 }
